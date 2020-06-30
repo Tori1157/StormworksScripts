43bf29d9d84096f650b8886e6f6dae89947935f2
Torkjell Thorsteinsson <torkjell.th@gmail.com>
2020-06-30 02:24:40 +0200

    Wrote new x16 average number output with alarm output and alarm toggle switch.
    Also added a nice little threshold checker, two inputs and one output.

---
+++ StormworksScripts/thresholdv1.lua
@@ -0,0 +1,10 @@
+﻿function onTick()
+	local value = input.getNumber(1)
+	local threshold = input.getNumber(2)
+
+	if value < threshold then
+		output.setBool(1, true)
+	else
+		output.setBool(1, false)
+	end
+end
