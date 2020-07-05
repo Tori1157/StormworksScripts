7985d7fef03765fc5924a8345513fe543f769a0b
Torkjell Thorsteinsson <torkjell.th@gmail.com>
2020-07-05 19:32:37 +0200

    Added script for getting a percentage of a input value.

---
+++ StormworksScripts/GetPercentage.lua
@@ -0,0 +1,10 @@
+﻿function onTick()
+	local value = input.getNumber(1)
+	local percentage = input.getNumber(2)
+	
+	output.setNumber(1, getPercentage(value, percentage))
+end
+
+function getPercentage(value, percentage)
+	return value * (percentage / 100)
+end
