ab5f6f126063a15926ba723d7c31bb37bb3baea6
Torkjell Thorsteinsson <torkjell.th@gmail.com>
2020-07-12 04:15:42 +0200

    Added new script used for composite in Stormwork, its also been optimized.

---
+++ StormworksScripts/Additions.lua
@@ -0,0 +1,11 @@
+﻿function onTick()
+	local total = 0
+	
+	for i = 15, 1, -1 do
+		local add = input.getNumber(i) or 0
+		
+		total = total + add
+	end
+	
+	output.setNumber(1, total)
+end
---
+++ StormworksScripts/CompositeToBool.lua
@@ -0,0 +1,43 @@
+﻿function onTick()
+	local doTooltip = property.getBool("Toggle Tooltip")
+
+	for i = 16, 1, -1 do
+		local boolInput = input.getBool(i)
+
+		if doTooltip then
+			local newNum = Switch(i)
+			output.setBool(newNum, boolInput)
+		end
+		
+		output.setBool(i, boolInput)
+	end
+end
+
+-- Using a table is 600% faster then doing 16 + i.
+function Switch(number)
+	local switchNums = {
+	[1] = 17,
+	[2] = 18,
+	[3] = 19,
+	[4] = 20,
+	[5] = 21,
+	[6] = 22,
+	[7] = 23,
+	[8] = 24,
+	[9] = 25,
+	[10] = 26,
+	[11] = 27,
+	[12] = 28,
+	[13] = 29,
+	[14] = 30,
+	[15] = 31,
+	[16] = 32
+	}
+
+	for key, value in ipairs(switchNums) do
+		if key == number then return value end
+	end
+	
+	-- Default, faster to send back same num than nil
+	return number
+end
