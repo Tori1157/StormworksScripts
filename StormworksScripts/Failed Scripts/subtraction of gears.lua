8c54bb3988f30056461602c5a8e954374e4cc6bc
Torkjell Thorsteinsson <torkjell.th@gmail.com>
2020-07-22 00:11:12 +0200

    Delete subtraction of gears.lua

--- StormworksScripts/Failed Scripts/subtraction of gears.lua
+++
@@ -1,9 +0,0 @@
-﻿function onTick()
-    local selectedGear = input.getNumber(1)
-    
-    if selectedGear > 7 then
-        selectedGear = -1
-    end
-    
-    output.setNumber(1, selectedGear)
-end
